c36d04e16742348428b72c06ccb6688645a6238a
Tova Jertfelt <tovizitan@gmail.com>
2022-02-14 19:17:47 +0100

    lagt in CSS (ej färdig version), lagt in en mobilmeny-toggle i javascript, samt skissat lite HTML (för att testa CSS:en)

---
+++ js/utilities.js
@@ -0,0 +1,13 @@
+///-----Menu button mobile
+let menuButton = document.getElementById("menuToggle");
+let menuMobile = document.querySelector("#dropDownMenu");
+
+menuButton.addEventListener("click", dropdown);
+
+function dropdown(){
+ if (menuMobile.classList.contains("drop-down"))
+ {menuMobile.classList.remove("drop-down");
+}
+else 
+menuMobile.classList.add("drop-down");
+}
