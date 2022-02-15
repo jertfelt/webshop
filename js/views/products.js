3ed39396d8bb49a6e29d8da55779e872f4acec90
Blanca Ley <blanca.ley@outlook.com>
2022-02-15 16:38:14 +0100

    Filter products for each category
    The app shows only the products for chosen category. The category selected depends on the URL Query String.
    Right now the URL is not updating. For now this works by manually entering the URL. Ex: http://127.0.0.1:5500/index.html?section=categoriesSection&category=Dam will get you only women's products.

--- js/views/products.js
+++ js/views/products.js
@@ -1,18 +1,23 @@
-//const productListSection = document.getElementById("productListSection");
+// This should work after fixing URL in changepage.js
+// It should get this URL: http://127.0.0.1:5500/index.html?section=categoriesSection&category=Dam
+const queryString = new URLSearchParams(location.search);
+const qsCategory = queryString.get('category');
 
 let productList = [];
 
 function drawProducts() {
   productList.forEach(product => {
+    if(product.category === qsCategory) {
     const article = document.createElement("article");
     article.innerHTML = `
       <a href="">
-        <img src="/${product.fields.image.fields.file.url}"></img>
+        <img style="width:100%" src="/${product.fields.image.fields.file.url}"></img>
         <h3>${product.fields.title}</h3>
         <p>${product.fields.price} kr</p>
       </a>
         <button>Köp</button>`
     productListSection.appendChild(article);
+    }
   })
 }
 
