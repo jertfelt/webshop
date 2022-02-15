f5744c528e60a6ad9011f20fc8b34ea90547f372
Blanca Ley <blanca.ley@outlook.com>
2022-02-15 15:00:41 +0100

    Add function to draw products titles dynamically
    The app draws the product's name dynamically. The data is fetched from products.json
    
    To Do: The function drawProducts will be able to draw a complete card for all products: name, price, image, b
    uy button.

--- js/views/products.js
+++ js/views/products.js
@@ -1,11 +1,21 @@
+//const productListSection = document.getElementById("productListSection");
+
 let productList = [];
+
+function drawProducts() {
+  productList.forEach(product => {
+    const article = document.createElement("article");
+    article.innerText = product.fields.title;
+    productListSection.appendChild(article);
+  })
+}
 
 async function getProductList() {
   const response = await fetch("/js/data/products.json");
   const data = await response.json();
   
   productList = [...data.products];
-  console.log(productList);
+  drawProducts();  
 }
 
 getProductList();
