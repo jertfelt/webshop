7002bb7d928da328c98ed2dfb4ee66a6136387f5
Blanca Ley <blanca.ley@outlook.com>
2022-02-15 11:54:41 +0100

    Change URL

--- js/views/main.js
+++ js/views/main.js
@@ -11,20 +11,29 @@ const loginSection = document.getElementById("loginSection");
 const createUserSection = document.getElementById("createUserSection");
 const cartSection = document.getElementById("cartSection"); 
 const buttonHero = document.getElementById("buttonHero");
+const sections = document.querySelectorAll(".section");
 
 //-addevent function som tooglar hidden/show + byter query string NAVBAR
 
+//Ändrar URL
 const changeActivePage = (queryString) => {
-  // const params = new URLSearchParams(location.search);
-  // params.set("section", queryString);
-  // params.toString();
-  // window.history.replaceState({}, '', ${location.pathname}?${params.toString()});
-  document.location = `?section=${queryString}`;
+
+  const url = new URL(window.location.href);
+  const search_params = url.searchParams;
+  search_params.set('section', queryString);
+  url.search = search_params.toString();
+
+  console.log(url.search)
+  sections.forEach(section => {
+    console.log(section);
+    //section.classList.add("hidden");
+  })
+
 };
 
-buttonHero.addEventListener("click", () => {
+
+
+buttonHero.addEventListener("click", (e) => {
   changeActivePage(3);
 });
-
-
 //loopa addeventen?
