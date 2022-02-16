c680c4edc6cdceb06915d285f2b273d64ac4aebc
emblafant <embla98wallen@gmail.com>
2022-02-16 12:27:56 +0100

    craeted function to create product element

--- js/views/orderConfirmation.js
+++ js/views/orderConfirmation.js
@@ -3,10 +3,40 @@
   /* 
   create product element
   grab information from local storage
+  print element on page
   show total price
   */
 
+  const createProductElementOrderConfirmation = (img, name, price, amount) => {
+    const productOrderConfirmation = document.createElement("article");
+    const productInformationDivOrderConfirmation = document.createElement("div");
+    const ImgOrderConfirmation = document.createElement("img");
+    const test = document.createElement("img")
 
+    const productNameOrderConfirmation = document.createElement("h3");
+    const priceOrderConfirmation = document.createElement("p");
+    const amountOrderConfirmation = document.createElement("p");
+
+    ImgOrderConfirmation.src = img;
+
+    productNameOrderConfirmation.innerText = name;
+    priceOrderConfirmation.innerText = price;
+    amountOrderConfirmation.innerText = `${amount} st`;
+
+    productInformationDivOrderConfirmation.appendChild(productNameOrderConfirmation);
+    productInformationDivOrderConfirmation.appendChild(priceOrderConfirmation);
+
+    productOrderConfirmation.appendChild(ImgOrderConfirmation);
+    productOrderConfirmation.appendChild(productInformationDivOrderConfirmation);
+    productOrderConfirmation.appendChild(amountOrderConfirmation);
+
+    console.log(productOrderConfirmation)
+
+    document.querySelector("#orderProductsSummation").appendChild(productOrderConfirmation);
+  }
+
+        /*
+        */
 
 //your details
 
