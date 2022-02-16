d567f63061edda406b2971024e6e7c949cba53c8
emblafant <embla98wallen@gmail.com>
2022-02-16 11:48:38 +0100

    added orderconfirmation js file

---
+++ js/views/orderConfirmation.js
@@ -0,0 +1,19 @@
+//show products
+
+  /* 
+  create product element
+  grab information from local storage
+  show total price
+  */
+
+
+
+//your details
+
+  /* 
+  check if logged in
+  if logged in check who is logged in
+  grab information from user and fill in form
+  
+  if not logged in do nothing
+  */
