5cb0d9f5200ea669d8be955075f310cec919d789
Celina Nylund <you@example.comcelina.nylund@gmail.com>
2022-02-17 08:27:19 +0100

    added categorys

--- js/views/categorys.js
+++ js/views/categorys.js
@@ -3,15 +3,30 @@
 fetch(`js/data/categories.json`)
 .then (response => response.json())
 .then(data =>{
-    console.log(data.categories[0].fields.image.fields.file.url)
+    const categoryDam = document.getElementById("categoryTitelInfoDam");
+    categoryDam.innerHTML= `
+        <h3>${data.categories[0].fields.title}</h3>
+        <h4>${data.categories[0].fields.description}</h4>
+    `;
+    const imgDam = document.getElementById("categoryImgDam");
+    imgDam.src = data.categories[0].fields.image.fields.file.url;
 
-    const h3 = document.getElementById("categoryTitle");
-    h3.innerHTML = data.categories[0].fields.title;
 
-    const h4 = document.getElementById("categoryInfo");
-    h4.innerHTML = data.categories[0].fields.description;
+    const categoryHerr = document.getElementById("categoryTitelInfoHerr");
+    categoryHerr.innerHTML= `
+        <h3>${data.categories[1].fields.title}</h3>
+        <h4>${data.categories[1].fields.description}</h4>
+    `;
+    const imgHerr = document.getElementById("categoryImgHerr");
+    imgHerr.src = data.categories[1].fields.image.fields.file.url;
 
-    const img = document.getElementById("categoryImg");
-    img.src = data.categories[0].fields.image.fields.file.url;
+
+    const categoryBaby = document.getElementById("categoryTitelInfoBaby");
+    categoryBaby.innerHTML= `
+        <h3>${data.categories[2].fields.title}</h3>
+        <h4>${data.categories[2].fields.description}</h4>
+    `;
+    const imgBaby = document.getElementById("categoryImgBaby");
+    imgBaby.src = data.categories[2].fields.image.fields.file.url;
 })
 
