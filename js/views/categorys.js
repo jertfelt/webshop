6a49db4b2a08e812c28143f7b4ea5463df89ee21
Celina Nylund <you@example.comcelina.nylund@gmail.com>
2022-02-21 10:46:00 +0100

    added button

--- js/views/categorys.js
+++ js/views/categorys.js
@@ -7,12 +7,17 @@ fetch(`js/data/categories.json`)
     categoryDam.innerHTML= `
         <h3>${data.categories[0].fields.title}</h3>
         <h4>${data.categories[0].fields.description}</h4>
+        <button id="categoryDamBtn">Se mer</button>
     `;
     const imgDam = document.getElementById("imgWrapperDam");
+    const btnDam = document.getElementById("categoryDamBtn");
     imgDam.innerHTML= `
         <img src="${data.categories[0].fields.image.fields.file.url}" class="product__img" alt="produkt ${data.categories[0].fields.title}">
     `;
     imgDam.onclick=()=>{
+        console.log("image clicked...");
+    }
+    btnDam.onclick=()=>{
         console.log("image clicked...");
     }
 
@@ -21,12 +26,17 @@ fetch(`js/data/categories.json`)
     categoryHerr.innerHTML= `
         <h3>${data.categories[1].fields.title}</h3>
         <h4>${data.categories[1].fields.description}</h4>
+        <button id="categoryHerrBtn">Se mer</button>
     `;
     const imgHerr = document.getElementById("imgWrapperHerr");
+    const btnHerr = document.getElementById("categoryHerrBtn");
     imgHerr.innerHTML= `
         <img src="${data.categories[1].fields.image.fields.file.url}" class="product__img" alt="produkt ${data.categories[1].fields.title}">
     `;
     imgHerr.onclick=()=>{
+        console.log("image clicked...");
+    }
+    btnHerr.onclick=()=>{
         console.log("image clicked...");
     }
 
@@ -35,12 +45,17 @@ fetch(`js/data/categories.json`)
     categoryBaby.innerHTML= `
         <h3>${data.categories[2].fields.title}</h3>
         <h4>${data.categories[2].fields.description}</h4>
+        <button id="categoryBabyBtn">Se mer</button>
     `;
     const imgBaby = document.getElementById("imgWrapperBaby");
+    const btnBaby = document.getElementById("categoryBabyBtn");
     imgBaby.innerHTML= `
         <img src="${data.categories[2].fields.image.fields.file.url}" class="product__img" alt="produkt ${data.categories[2].fields.title}">
     `;
     imgBaby.onclick=()=>{
+        console.log("image clicked...");
+    }
+    btnBaby.onclick=()=>{
         console.log("image clicked...");
     }
 })
