be1a5f86e7f8b8f76bb4ff2e7913b18dca022e98
Tova Jertfelt <tovizitan@gmail.com>
2022-02-23 16:39:04 +0100

    manually copied receipt.js

--- js/views/receipt.js
+++ js/views/receipt.js
@@ -1,41 +1,44 @@
+//all infofrån localStorage
+
+let orderInformationReceipt = JSON.parse(localStorage.getItem("orderInformation"));
+
+//Användarens köpta produkter
+
+if (getCart() !== null) {
+    getCart().forEach(product => {
+        let productName = document.createElement("li");
+        productName.innerHTML = `${product.quantity} x ${product.fields.title}`;
+        const productUl = document.getElementById("productList");
+        productUl.appendChild(productName);
+    });
+  }
 
 
-// let infoArray =[];
-
-// if (localStorage.length!== 0){
-//     for (let i=0; i<localStorage.length; i++){
-//         let key = localStorage.key(i);
-//         infoArray.push(JSON.parse(localStorage.getItem(key)));
-//     }
-// }
-// console.log(infoArray);
-
-
-// submitBtn.addEventListener(("click"), ()=>{
-
-// })
-
-//Hämta localStorage 
-  
-const submitBtn = document.getElementById("submitBtnOrderConfirmation");
-
-submitBtn.addEventListener("submit", (e)=>{
+//självaste kvittot som renderas ut
+document.getElementById("orderConfirmationForm").addEventListener("submit", (e)=>{
     e.preventDefault();
 
-    document.getElementById("").innerHTML = `
+    document.getElementById("receiptDiv").innerHTML = `
     <div class="kvitto__container">
+
         <div class="kvitto__details">
-            <p>"${}"</p>
-            <p>"${}"</p>
-            <p>"${}"</p>
-            <p>"${}"</p>
+            <b>Dina detaljer:</b>
+            <p>${orderInformationReceipt.name}</p>
+            <p>${orderInformationReceipt.street}</p>
+            <p>${orderInformationReceipt.postalCode} ${orderInformationReceipt.town}</p>
+            <p>${orderInformationReceipt.phone}</p>
+            <p>${orderInformationReceipt.email}</p>
         </div>
+
         <div class="kvitto__products">
+        <b>Dina produkter:</b>
+        <ul id="productList"></ul>
         </div>
+
         <div class="kvitto__total">
-        <p>Totalt:"${}" SEK</p>
+        <p>Totalt: ${getTotalPriceOrder()} SEK</p>
         <p>Tack för ditt köp!</p>
         </div>
-    </div>
-    `;
-})
+        </div>
+        `;
+    })
