58b00c1328f8384a71f5ed2d46ac287734443939
hanifb1360 <hanif.bahari@gmail.com>
2022-02-24 18:07:51 +0100

    new function for button

--- js/views/searchBar.js
+++ js/views/searchBar.js
@@ -27,70 +27,23 @@ const loadSearchResult = async () => {
 };
 
 const displaySearchResult = (res, source) => {
+  const sectionElem = document.createElement("section");   
+  sectionElem.classList.add("products__grid--all");   
+  searchSection.appendChild(sectionElem);
 
   //mapping all products and checking if the product title includes the input word
+  
   const htmlString = res.products
     .map((produx) => {
       if (produx.fields.title.toLowerCase().includes(source.toLowerCase()) || produx.fields.description.toLowerCase().includes(source.toLowerCase())) {
         
         
-        
+        createProductCard(produx, sectionElem);}})
       
-        //returning the products that match
-        const productLink = createURL(
-          "individualProductSection",
-          `${produx.category}`,
-          `${produx.sys.id}`
-        );
-          //replacing all sections
-        const produxPage = document.querySelector(".section")
-      
-        produxPage.innerHTML = "";
-        return `
-        <article class="products__search">
-            
-            <div class=" product__img--container product__img--list">
-              <a href="${productLink}">
-                <img 
-                  alt= "Produkt"
-                  src="/${produx.fields.image.fields.file.url}">
-                </img>
-              <a href="${productLink}">
-            </div>
-            <a href="${productLink}">
-            
-            </a>
-            <span class="product__box--search">
-              <span class="row">
-                <a href="${productLink}">
-                  <h3 
-                    class="text--uppercase text--cursive">
-                    ${produx.fields.title}
-                    </h3>
-                </a>
-                    <h4 class="text--green">${produx.fields.price} kr
-                    </h4>
-              </span>
-                  <p 
-                  class="text--centered">
-                  ${produx.fields.description} 
-                  
-                  </p>
-               </span>
-              
-          </article>
-
-        `;
-      }
-    })
-    .join("");
-  const searchResults = document.getElementById("searchSection");
-  searchResults.innerHTML = htmlString;
+  
 };
 
-// // <button class="addToCartBtn " 
-// data-id="${produx.sys.id}">Köp
-// </button>
+
 
 
 loadSearchResult();
