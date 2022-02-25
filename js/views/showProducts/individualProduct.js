ff9bbcbaa7d3e3111cf12ecea3da0afb9eaa0893
Blanca Ley <blanca.ley@outlook.com>
2022-02-25 13:11:16 +0100

    Simplify logic inside getProduct function

--- js/views/showProducts/individualProduct.js
+++ js/views/showProducts/individualProduct.js
@@ -1,38 +1,32 @@
 const queryParams = new URLSearchParams(location.search);
 const productId = queryParams.get('id');
 
-let productsArray = [];
+// let allProductsArray = [];
 
 const productCreator = (product)=>{
-
-   
-    const sectionProduct = document.createElement("section");
-    sectionProduct.classList.add("products__individual");
-    individualProductSection.appendChild(sectionProduct);
-    
-    const articleProduct = document.createElement("article");
-
-    articleProduct.innerHTML = `
-        <img alt="Produkt"
-        src="/${product.fields.image.fields.file.url}">
-        </img>
-      <span class="product__box--individual">
-      <span class="row">
-          <h3>${product.fields.title}</h3>
-          <p class="text--s text--green text--bold">
-          ${product.fields.price} kr </p>
-        </span>
-         
-          <p class="text--s">${product.fields.description}</p>
-          <button class="addToCartBtn " 
-          data-id="${product.sys.id}">Köp
-          </button>
-          
-          
-      </span>
-    `
+  const sectionProduct = document.createElement("section");
+  sectionProduct.classList.add("products__individual");
+  individualProductSection.appendChild(sectionProduct);
   
-    sectionProduct.appendChild(articleProduct);
+  const articleProduct = document.createElement("article");
+
+  articleProduct.innerHTML = `
+    <img alt="Produkt"
+    src="/${product.fields.image.fields.file.url}">
+    </img>
+    <span class="product__box--individual">
+    <span class="row">
+      <h3>${product.fields.title}</h3>
+      <p class="text--s text--green text--bold">
+      ${product.fields.price} kr </p>
+    </span>
+    <p class="text--s">${product.fields.description}</p>
+    <button class="addToCartBtn " 
+    data-id="${product.sys.id}">Köp
+    </button>
+    </span>
+  `
+  sectionProduct.appendChild(articleProduct);
 }
 
 // Wrapper funktion som kör funktioner/eventListeners efter HTML i produktlistan har ritats
@@ -51,7 +45,7 @@ const setAddToCartClickProduct = (productList) => {
 }
 
 async function getProduct() {  
-  const allProductsArray = await fetchProducts();
+  allProductsArray = await fetchProducts();
   allProductsArray.map((product) => {
     if (product.sys.id === productId){
         productCreator(product)
