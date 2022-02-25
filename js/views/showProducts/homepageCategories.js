ab1a3f16d258852e17fa68add072fb56c82caa1f
Celina Nylund <you@example.comcelina.nylund@gmail.com>
2022-02-25 20:44:53 +0100

    added forEach-loop on categoryArray, new work is commented, not done jet

--- js/views/showProducts/homepageCategories.js
+++ js/views/showProducts/homepageCategories.js
@@ -5,9 +5,7 @@ const categoryId = queryParams1.get('id');
 
 let categoryArray = []; 
 
-const categoryCreator = (data) => {
-
-
+const drawCategorys = (data) => {
     categoriesSection.innerHTML= `
     <article id="categoryDam" class="grid__category--items">
     <div class="pointer  product__img--container">
@@ -44,20 +42,20 @@ const categoryCreator = (data) => {
     </article>
         `;
 
-        const categoryDamClick = document.getElementById("categoryDam");
-        categoryDamClick.onclick=()=>{
-            changeActivePage("productListSection", "Dam");
-        }
+    const categoryDamClick = document.getElementById("categoryDam");
+    categoryDamClick.onclick=()=>{
+        changeActivePage("productListSection", "Dam");
+    }
 
-        const categoryHerrClick = document.getElementById("categoryHerr");
-        categoryHerrClick.onclick=()=>{
-            changeActivePage("productListSection", "Herr");
-        }
+    const categoryHerrClick = document.getElementById("categoryHerr");
+    categoryHerrClick.onclick=()=>{
+        changeActivePage("productListSection", "Herr");
+    }
 
-        const categoryBabyClick = document.getElementById("categoryBaby");
-        categoryBabyClick.onclick=()=>{
-            changeActivePage("productListSection", "Baby");
-        }
+    const categoryBabyClick = document.getElementById("categoryBaby");
+    categoryBabyClick.onclick=()=>{
+        changeActivePage("productListSection", "Baby");
+    }
     }
 
 //---------fetch from JSON and render:
@@ -73,7 +71,66 @@ async function getCategories() {
     const categoryTitle = productsArray.map(x=>{
         return x;
     })
-    categoryCreator(categoryTitle);
+    drawCategorys(categoryTitle);
 }
 
 getCategories();
+
+// const categoriesSection = document.querySelector(".products__grid--categories");
+
+// const queryParams1 = new URLSearchParams(location.search);
+// const categoryId = queryParams1.get('id');
+
+// let categoryArray = []; 
+
+// const drawCategorys = (data) => {
+//     data.forEach(x =>{
+//         let tempArt = document.createElement("article");
+//         tempArt.innerHTML=`
+//         <article id="categoryDam" class="grid__category--items">
+//         <div class="pointer  product__img--container">
+//         <img src="${x.fields.image.fields.file.url}" class="product__img" 
+//         alt="${x.fields.title}"></img>
+//         <span class="row">
+//         <h3 class="text--green text--cursive">${x.fields.title}</h3>
+//         <h4>${x.fields.description}</h4>
+//         </span>
+//         </div>
+//         </article>`;
+        
+//         categoriesSection.appendChild(tempArt);
+        
+//     })
+//         const categoryDamClick = document.getElementById("categoryDam");
+//         categoryDamClick.onclick=()=>{
+//             changeActivePage("productListSection", "Dam");
+//         }
+
+//         const categoryHerrClick = document.getElementById("categoryHerr");
+//         categoryHerrClick.onclick=()=>{
+//             changeActivePage("productListSection", "Herr");
+//         }
+
+//         const categoryBabyClick = document.getElementById("categoryBaby");
+//         categoryBabyClick.onclick=()=>{
+//             changeActivePage("productListSection", "Baby");
+//         }
+//     }
+
+// //---------fetch from JSON and render:
+
+// async function getCategories() {
+
+//     const catResponse = await fetch("/js/data/categories.json");
+
+//     const data = await catResponse.json();
+
+//     categoryArray = [...data.categories];
+
+//     const categoryTitle = categoryArray.map(x=>{
+//         return x;
+//     })
+//     drawCategorys(categoryTitle);
+// }
+
+// getCategories();
